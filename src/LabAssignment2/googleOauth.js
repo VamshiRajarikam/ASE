30c615c3f3ce6d3999b752439203721abe5e1760
VamshiRajarikam <vamshi.rajarikam@gmail.com>
2017-02-08 00:18:01 -0600

    screenshots

---
+++ src/LabAssignment2/googleOauth.js
@@ -0,0 +1,25 @@
+<script src="https://apis.google.com/js/api.js"></script>
+<script>
+function start() {
+  // 2. Initialize the JavaScript client library.
+  gapi.client.init({
+    'apiKey': 'YOUR_API_KEY',
+    'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
+    // clientId and scope are optional if auth is not required.
+    'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+    'scope': 'profile',
+  }).then(function() {
+    // 3. Initialize and make the API request.
+    return gapi.client.people.people.get({
+      resourceName: 'people/me'
+    });
+  }).then(function(response) {
+    console.log(response.result);
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
+};
+// 1. Load the JavaScript client library.
+gapi.load('client', start);
+</script>
+    
