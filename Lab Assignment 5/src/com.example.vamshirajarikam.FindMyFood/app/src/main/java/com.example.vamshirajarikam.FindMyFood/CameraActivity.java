f89705f12ab64cead22085801bb9694c20d7775b
VamshiRajarikam <vamshi.rajarikam@gmail.com>
2017-02-22 23:31:58 -0600

    assignment5

---
+++ Lab Assignment 5/src/com.example.vamshirajarikam.FindMyFood/app/src/main/java/com.example.vamshirajarikam.FindMyFood/CameraActivity.java
@@ -0,0 +1,37 @@
+package com.example.vamshirajarikam.FindMyFood;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CameraActivity extends Activity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView2);
+        Button photoButton = (Button) this.findViewById(R.id.btn_camera);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+}
