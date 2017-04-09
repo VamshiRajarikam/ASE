a6358059c79255c271ca7f9ecc7dbf2f4ca38b0a
VamshiRajarikam <vamshi.rajarikam@gmail.com>
2017-04-08 23:35:28 -0500

    updated

--- Lab Assignment 10/src/Gruntfile.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-    });
-};
---
+++ Lab Assignment 10/src/Tutorial 10 - Node JS & Mongo DB/MogoDBConnection/MongoConnection.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mnpw3d on 20/10/2016.
+ */
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url ='mongodb://Vamshi:9848663776@ds139370.mlab.com:39370/asedatabase';
+//var url = 'mongodb://marmik:2621@ds051923.mlab.com:51923/demo';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
